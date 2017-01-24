f8d28aeec0f959f36df2e6e85f948ec55be86180
DESKTOP-O5P82QB\nicke <nickeblewis@gmail.com>
2017-01-24 15:31:46 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ListPage from './components/ListPage'
+import CreatePage from './components/CreatePage'
+import { Router, Route, browserHistory } from 'react-router'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import 'tachyons'
+import './index.css'
+
+const networkInterface = createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cixraxev60e4c0121krsia44h' })
+
+// The x-graphcool-source header is to let the server know that the example app has started.
+// (Not necessary for normal projects)
+networkInterface.use([{
+  applyMiddleware (req, next) {
+    if (!req.options.headers) {
+      // Create the header object if needed.
+      req.options.headers = {}
+    }
+    req.options.headers['x-graphcool-source'] = 'example:react-apollo-instagram'
+    next()
+  },
+}])
+
+const client = new ApolloClient({ networkInterface })
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <Router history={browserHistory}>
+      <Route path='/' component={ListPage} />
+      <Route path='/create' component={CreatePage} />
+    </Router>
+  </ApolloProvider>
+  ),
+  document.getElementById('root')
+)
