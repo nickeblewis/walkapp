4f280b564ed4a515e4384155571303ead0aaf7f8
Nick Lewis <Nick.Lewis@pcl.co.uk>
2017-01-25 16:16:12 +0000

    Dans code added though slightly broken

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -14,8 +14,26 @@ class HomePage extends React.Component {
 
   render () {
     return (
-      <div className='w-100 flex justify-center'>
-        We shall add something here...
+      <div className="bg-green white-90 pa4">
+        <div className='w-100 flex justify-center'>
+          <div className="ba pa2 br4 bw1 bg-black-60 b--solid fixed">
+            <h2 className="i f4 tl">Demo Site</h2>
+          </div>
+          <div className="ba pa2 bw1 bg-black-80 b--solid tr">
+            <Link src="file:///C:/Users/Dan%20Davis%20(Work)/demo/demo/Contact%20Page.html" className="link dim light-silver">Contact</Link>
+          </div>
+          <h1 className="f1 tc underline">Walks</h1>
+          <p className="underline f3 b">Select Your Difficulty:</p>
+          <div className="ba bw2 b--solid pa1 tc br4">
+              <p>Easy</p>
+          </div>        
+          <div className=" fw4 ba bw2 b--solid pa1 tc br4">
+              <p>Medium</p>
+          </div>
+          <div className="ba bw2 b--solid pa1 tc br4">
+            <p>Hard</p>
+          </div>
+        </div>
       </div>
     )
   }
