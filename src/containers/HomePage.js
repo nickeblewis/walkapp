f7ac9e443e4c09b00eddcf36a18573be44445257
DESKTOP-O5P82QB\nicke <nickeblewis@gmail.com>
2017-02-04 17:16:23 +0000

    Added react bootstrap

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -6,6 +6,8 @@ import React from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
+import { Grid, Thumbnail, Col, Row, Button, Jumbotron } from 'react-bootstrap'
+
 class HomePage extends React.Component {
 
   static propTypes = {
@@ -14,9 +16,45 @@ class HomePage extends React.Component {
 
   render () {
     return (
-      <div>
-        <h2>What is really not going on?</h2>
-      </div>
+       <Grid>
+        <Jumbotron>
+          <h1>Farnborough Guide</h1>
+          <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <p><Button bsStyle="primary">Learn more</Button></p>
+        </Jumbotron>
+        <Row>
+          <Col xs={6} md={4}>
+            <Thumbnail src="http://placehold.it/242x200" alt="242x200">
+              <h3>Thumbnail label</h3>
+              <p>Description</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+          <Col xs={6} md={4}>
+            <Thumbnail src="http://placehold.it/242x200" alt="242x200">
+              <h3>Thumbnail label</h3>
+              <p>Description</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+          <Col xs={6} md={4}>
+            <Thumbnail src="http://placehold.it/242x200" alt="242x200">
+              <h3>Thumbnail label</h3>
+              <p>Description</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
