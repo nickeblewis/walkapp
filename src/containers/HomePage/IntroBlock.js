52cae3eccac5d78182c0d29d3080071f5044e106
Nick Lewis <nick@headforcode.com>
2017-02-20 21:22:05 +0000

    edited intro block

--- src/containers/HomePage/IntroBlock.js
+++ src/containers/HomePage/IntroBlock.js
@@ -1,6 +1,7 @@
 // Always import React at the beginning of every React Component
 // Everything is a component in React!
 import React from 'react'
+import { Link } from 'react-router'
 
 // https://ponyfoo.com/articles/es6-classes-in-depth describes classes really well
 class IntroBlock extends React.Component {
@@ -18,21 +19,25 @@ class IntroBlock extends React.Component {
 
     // Now for the JSX template that defines how our component actually looks!
     return (
-      <div className="cf">
+      <div className="db dt-ns mw9 center w-100">
         <div className="fl-m fl-l w-50-m w-50-l br b--light-gray">
-          <div className="pa3 pa4-m pa5-l">
-            <h3 className="f3">Businesses</h3>
+          <div className="pa3 pa4-m">
+            <h3 className="f3">Welcome</h3>
             <p className="mid-gray f3 lh-copy measure-narrow">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut iaculis mi. In facilisis risus ante, ac aliquet diam porttitor vitae. Sed libero diam, cursus a augue eget, molestie dignissim metus
+              Farnborough Guide is a new service that aims to help businesses promote themselves to local residents and other businesses alike. The site will also help people find out about interesting services, events, places, activities and a whole lot more.
+            </p>
+            <p className="mid-gray f4 lh-copy measure-narrow">
+              We are launching the site early by introducing our local photography campaign. We invite everyone to share their photgraphs of the town here on Farnborough Guide. We are especially interested in businesses around the town whom wish to share photographs of what they do, what they sell. help spread the word.
             </p>
           </div>
         </div>
         <div className="fl-m fl-l w-50-m w-50-l">
-          <div className="pa3 pa4-m pa5-l">
-            <h3 className="f3">Events</h3>
+          <div className="pa3 pa4-m">
+            <h3 className="f3">Sign-up</h3>
             <p className="mid-gray f3 lh-copy measure-narrow">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut iaculis mi. In facilisis risus ante, ac aliquet diam porttitor vitae. Sed libero diam, cursus a augue eget, molestie dignissim metus
+              So you have some photos to share with everyone in the town? What are you waiting for? Once registered you may upload new images via our photos page!
             </p>
+            <Link className="f3 fw4 hover-red no-underline black-70 dib pv2 ph3 ba" to="/signup" >Register Now!</Link> 
           </div>
         </div>
       </div>
