35aa68ce85f49773acfab732d022bd221d09c5cc
Nick Lewis <nickeblewis@gmail.com>
2017-02-24 11:38:32 +0000

    instructions for Dan

---
+++ docs/changes/intro-block.md
@@ -0,0 +1,19 @@
+We want to change the layout of the introduction text that appears 
+on the homepage. I shall explain how we want it to be restructured.
+
+First of all though...
+
+Grab all of the latest changes and create a new branch to work from:
+
+* git pull for the latest changes made by myself
+* git checkout -b "introblockchanges"
+
+We shall be working with just one file for this piece of work and 
+that is IntroBlock.js, you can get to it quickly in VSC by pressing 
+CTRL+P which reveals the search drop-down. Type in "IntroBlock.js" and select
+it to open the file in VSC.
+
+I have added some comments to the above file, so go over there to 
+learn more about what needs doing :-)
+
+
--- src/containers/HomePage/IntroBlock.js
+++ src/containers/HomePage/IntroBlock.js
@@ -1,36 +1,39 @@
-// Always import React at the beginning of every React Component
-// Everything is a component in React!
 import React from 'react'
 import { Link } from 'react-router'
 
-// https://ponyfoo.com/articles/es6-classes-in-depth describes classes really well
 class IntroBlock extends React.Component {
 
-  // This component is the child of another and we can pass properties down the chain from a parent 
-  // to its child components (children)
-  // in this case we are defining data as being a mandatory property which this component can't run without
   static propTypes = {
     data: React.PropTypes.object,
   }
-
-  // The following function is responsible for rendering to the browser
+  
+  /**
+   * We need to rearaange this code a bit so that the Welcome section is one wide column and the sections
+   * entitled photography and sign up are in tow columns below, so something like:
+   * |     Welcome     |
+   * | Photo | Signup  |
+   */
   render () {
-
-
-    // Now for the JSX template that defines how our component actually looks!
     return (
       <div className="db dt-ns mw9 center w-100">
+        {/* Insert a new column here, my commented out code below will give you some help */}
+        {/*<div className="fl-m fl-l w-50-m w-50-l br b--light-gray">
+
+        </div>*/}
         <div className="fl-m fl-l w-50-m w-50-l br b--light-gray">
           <div className="pa3 pa4-m">
+            {/*This section needs to be moved up to the new column*/}
             <h3 className="f3">Welcome</h3>
             <p className="mid-gray f3 lh-copy measure-narrow">
               Farnborough Guide is a new service that aims to help businesses promote themselves to local residents and other businesses alike. The site will also help people find out about interesting services, events, places, activities and a whole lot more.
             </p>
+            {/*End of section to copy*/}
+            <h3 className="f3">Photography</h3>
             <p className="mid-gray f4 lh-copy measure-narrow">
               We are launching the site early by introducing our local photography campaign. We invite everyone to share their photgraphs of the town here on Farnborough Guide. We are especially interested in businesses around the town whom wish to share photographs of what they do, what they sell. help spread the word.
             </p>
           </div>
-        </div>
+        </div>        
         <div className="fl-m fl-l w-50-m w-50-l">
           <div className="pa3 pa4-m">
             <h3 className="f3">Sign-up</h3>
