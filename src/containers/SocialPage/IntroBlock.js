ac5212c1a45c3bbf3beb161c519401d449a950b4
danheadforcode <dan@headfocode.com>
2017-07-31 11:19:55 +0100

    all the logos done playing around with sizing now

--- src/containers/SocialPage/IntroBlock.js
+++ src/containers/SocialPage/IntroBlock.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router'
 import instagram_logo from '../../assets/instagram_logo.png'
+import facebook_logo from '../../assets/facebook_logo.png'
+import snapchat_logo from '../../assets/snapchat_logo.png'
+import twitter_logo from '../../assets/twitter_logo.png'
 
 class IntroBlock extends React.Component {
 
@@ -15,6 +18,7 @@ class IntroBlock extends React.Component {
         <div className="fl-m fl-l w-50-m w-50-l">
           <div className="pa4 pa4-m">
           <h3 className="f3">Facebook</h3>
+            <img src={facebook_logo}/>
             <p className="mid-gray f4 lh-copy">            
                
             </p> 
@@ -23,8 +27,8 @@ class IntroBlock extends React.Component {
         <div className="fl-m fl-l w-50-m w-50-l">
           <div className="pa4 pa4-m">
             <h3 className="f3">Instagram</h3>
-            <div>
-              <img clasaName="positon" src={instagram_logo}/>
+            <div clasaName="size">
+              <img src={instagram_logo}/>
             </div>
             <p className="mid-gray f4 lh-copy measure-narrow">
 
@@ -34,6 +38,7 @@ class IntroBlock extends React.Component {
         <div className="fl-m fl-l w-50-m w-50-l">
           <div className="pa4 pa4-m">
             <h3 className="f3">Snapchat</h3>
+              <img src={snapchat_logo}/>
             <p className="mid-gray f4 lh-copy measure-narrow">
 
             </p>
@@ -42,6 +47,7 @@ class IntroBlock extends React.Component {
         <div className="fl-m fl-l w-50-m w-50-l br b--light-gray">
           <div className="pa4 pa4-m">
             <h3 className="f3">Twitter</h3>
+              <img src={twitter_logo}/>
             <p className="mid-gray f4 lh-copy measure-narrow">
 
             </p>
