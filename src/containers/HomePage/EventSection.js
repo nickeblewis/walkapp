efbf9e9483d30912f42025fc5bfedf3a1534ad5d
Nick Lewis <nickeblewis@gmail.com>
2017-11-02 15:38:17 +0000

    Fixed data query bug that was pulling in the now deleted field imageUrl

--- src/containers/HomePage/EventSection.js
+++ src/containers/HomePage/EventSection.js
@@ -53,7 +53,6 @@ class EventSection extends React.Component {
 const FeedQuery = gql`query allEvents {
   allEvents(orderBy: createdAt_DESC, first: 5) {
     id
-    imageUrl
     publicId
     name
     slug
