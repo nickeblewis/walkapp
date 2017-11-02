e49b18d881e75f45ae77883d3d04ca87c07e8022
Nick Lewis <nickeblewis@gmail.com>
2017-11-02 15:05:54 +0000

    Now that is looking much better

--- src/containers/EventPage.js
+++ src/containers/EventPage.js
@@ -7,9 +7,17 @@ import gql from 'graphql-tag'
 import { CloudinaryContext, Transformation, Image } from 'cloudinary-react'
 // import { Link } from 'react-router'
 import { withRouter } from 'react-router'
+import { Map, Marker, TileLayer } from 'react-leaflet'
 
 class EventPage extends React.Component {
-
+  constructor() {
+    super();
+    this.state = {      
+      lat: 51.27985,
+      lng: -0.75159,
+      zoom: 15,
+    };
+  }
   static propTypes = {
     data: React.PropTypes.shape({
       loading: React.PropTypes.bool,
@@ -45,11 +53,12 @@ class EventPage extends React.Component {
       console.log(this.props.data.Event)
     // const Event = this.props.data.Event
 
+    let position = [this.props.data.Event.eventVenue.longitude,this.props.data.Event.eventVenue.latitude]
     return (
       <main>
-        <div className="db dt-ns mw9 center">
+        <div className="db dt-ns mw9 center bg-black">
           <div className="fl-m fl-l w-50-m w-50-l">
-            <div className="pa4 pa4-m">
+            <div className="pa4 pa4-m center">
                   <CloudinaryContext cloudName="dqpknoetx">
                   <Image publicId={this.props.data.Event.publicId}>
                     <Transformation width="500" crop="scale" />
@@ -59,28 +68,46 @@ class EventPage extends React.Component {
                 
           </div>
         </div>
-        <div className="fl-m fl-l w-50-m w-50-l">
+        <div className="fl-m fl-l w-50-m w-50-l bg-near-white">
           <div className="pa4 pa4-m">
-            <h3 className="f4">{this.props.data.Event.name}</h3>
-            <p className="mid-gray f5 lh-copy">
+            <h3 className="f2">{this.props.data.Event.name}</h3>
+            <p className="mid-gray f4 lh-copy">
               {this.props.data.Event.description}
             </p>
             <h3 className="f4">Where?</h3>
-            <p className="mid-gray f5 lh-copy">
+            <Map 
+        center={position} 
+        zoom={this.state.zoom} 
+        scrollWheelZoom={false}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+          <Marker position={position}>
+            
+          </Marker>        
+      </Map>
+            <p className="mid-gray f4 lh-copy">
               {this.props.data.Event.eventVenue.title}
             </p>
-            <h3 className="f4">When?</h3>
+            <p className="mid-gray f6 lh-copy">
+              {this.props.data.Event.eventVenue.address}
+            </p>
+            <p className="mid-gray f6 lh-copy">
+              {this.props.data.Event.eventVenue.postcode}
+            </p>
+            {/* <h3 className="f4">When?</h3>
             <p className="mid-gray f5 lh-copy">
               {this.props.data.Event.eventDate}
-            </p>
-            <h3 className="f4">Who to Contact?</h3>
+            </p> */}
+            <h3 className="f4">Contact Details</h3>
             <p className="mid-gray f5 lh-copy">
               {this.props.data.Event.contactName}
             </p>
-            <h3 className="f4">Cost</h3>
+            {/* <h3 className="f4">Cost</h3>
             <p className="mid-gray f5 lh-copy">
               {this.props.data.Event.cost}
-            </p>
+            </p> */}
           </div>
         </div>
       </div>
@@ -103,7 +130,11 @@ query EventQuery($id: String!) {
       slug
       eventVenue {
         title
-      }
+        postcode
+        address
+        latitude
+        longitude
+      }      
       eventDate
     }
   }
