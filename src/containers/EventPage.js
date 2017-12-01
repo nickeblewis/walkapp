b6c00ac598d62e9fa53adb9470f731050c105625
Nick Lewis <nickeblewis@gmail.com>
2017-12-01 17:11:50 +0000

    Map setting for GPS

--- src/containers/EventPage.js
+++ src/containers/EventPage.js
@@ -12,7 +12,7 @@ import { Map, Marker, TileLayer } from 'react-leaflet'
 class EventPage extends React.Component {
   constructor() {
     super();
-    this.state = {      
+    this.state = {
       lat: 51.27985,
       lng: -0.75159,
       zoom: 15,
@@ -28,88 +28,86 @@ class EventPage extends React.Component {
     params: React.PropTypes.object.isRequired
   }
 
-  
-  render () {
+
+  render() {
     if (this.props.data.loading) {
       return (<div>Loading</div>)
     }
 
-     if (this.props.data.error) {
+    if (this.props.data.error) {
       console.log(this.props.data.error)
       return (<div>An unexpected error occurred</div>)
     }
 
-      console.log(this.props.data.Event)
-      
-      const Event = this.props.data.Event
+    console.log(this.props.data.Event)
 
-    let position = [Event.place.longitude,Event.place.latitude]
+    const Event = this.props.data.Event
+
+    let position = [Event.lat, Event.lng]
     return (
       <main>
         <div className="db dt-ns mw9 center bg-black">
           <div className="fl-m fl-l w-50-m w-50-l">
             <div className="pa4 pa4-m center">
-                  <CloudinaryContext cloudName="dqpknoetx">
-                  <Image publicId={Event.publicId}>
-                    <Transformation width="500" crop="scale" />
-                  </Image>
-                </CloudinaryContext>
+              <CloudinaryContext cloudName="dqpknoetx">
+                <Image publicId={Event.publicId}>
+                  <Transformation width="500" crop="scale" />
+                </Image>
+              </CloudinaryContext>
+            </div>
+          </div>
+          <div className="fl-m fl-l w-50-m w-50-l bg-near-white">
+            <div className="pa4 pa4-m">
+              <h3 className="f2">{Event.name}</h3>
+              <p className="mid-gray f4 lh-copy">
+                {Event.description}
+              </p>
+              <h3 className="f4">Where?</h3>
+              <Map
+                center={position}
+                zoom={this.state.zoom}
+                scrollWheelZoom={false}>
+                <TileLayer
+                  url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={position}>
 
-                
+                </Marker>
+              </Map>
+              {/* <p className="mid-gray f4 lh-copy">
+                {Event.place.title}
+              </p>
+
+              <p className="mid-gray f6 lh-copy">
+                {Event.place.address}
+              </p>
+
+              <p className="mid-gray f6 lh-copy">
+                {Event.place.postcode}
+              </p> */}
+
+              <h3 className="f4">Contact Details</h3>
+
+              <p className="mid-gray f5 lh-copy">
+                {Event.contactName}
+              </p>
+
+              <p className="mid-gray f5 lh-copy">
+                <a href={"mailto:" + Event.contactEmail}>{Event.contactEmail}</a>
+              </p>
+
+              <p className="mid-gray f5 lh-copy">
+                <a target="_blank" href={"http://" + Event.website}>{Event.website}</a>
+              </p>
+              {/* <p className="mid-gray f5 lh-copy">
+                {Event.place.phone ? Event.place.phone : "No phone number available"}
+              </p> */}
+            </div>
           </div>
         </div>
-        <div className="fl-m fl-l w-50-m w-50-l bg-near-white">
-          <div className="pa4 pa4-m">
-            <h3 className="f2">{Event.name}</h3>
-            <p className="mid-gray f4 lh-copy">
-              {Event.description}
-            </p>
-            <h3 className="f4">Where?</h3>
-            <Map 
-        center={position} 
-        zoom={this.state.zoom} 
-        scrollWheelZoom={false}>
-        <TileLayer
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-          <Marker position={position}>
-            
-          </Marker>        
-      </Map>
-            <p className="mid-gray f4 lh-copy">
-              {Event.place.title}
-            </p>
-
-            <p className="mid-gray f6 lh-copy">
-              {Event.place.address}
-            </p>
-
-            <p className="mid-gray f6 lh-copy">
-              {Event.place.postcode}
-            </p>
-
-            <h3 className="f4">Contact Details</h3>
-
-            <p className="mid-gray f5 lh-copy">
-              {Event.contactName}              
-            </p>
-
-            <p className="mid-gray f5 lh-copy">
-              <a href={"mailto:" + Event.contactEmail}>{Event.contactEmail}</a>
-            </p>
-
-            <p className="mid-gray f5 lh-copy">
-              <a target="_blank" href={"http://" + Event.website}>{Event.website}</a>
-            </p>
-            <p className="mid-gray f5 lh-copy">
-              {Event.place.phone ? Event.place.phone : "No phone number available"}
-            </p>
-          </div>
-        </div>
-      </div>
-            <div className="pa4 ph7-l georgia mw9-l center"></div>
-        </main>
+        <div className="pa4 ph7-l georgia mw9-l center"></div>
+      </main>
     )
   }
 }
@@ -125,6 +123,8 @@ query EventQuery($id: String!) {
       contactEmail
       cost
       slug
+      lat
+      lng
       place {
         title
         postcode
@@ -139,7 +139,7 @@ query EventQuery($id: String!) {
   }
 `
 
-const EventPageWithQuery = graphql(EventQuery,{
+const EventPageWithQuery = graphql(EventQuery, {
   options: (ownProps) => ({
     variables: {
       id: ownProps.params.id
