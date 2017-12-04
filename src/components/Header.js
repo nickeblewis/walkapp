ccc673e69af83e87becd8b986554bac0c7a569d3
Nick Lewis <nickeblewis@gmail.com>
2017-12-04 09:56:50 +0000

    work in progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,32 +3,50 @@
  */
 import React from 'react'
 // import { Link } from 'react-router'
+// import bg from '../assets/NIC_0713.jpg'
 
 class Header extends React.Component {
 
+    render = () => {
+        return (
+            // <section className="hero is-warning " style={{ backgroundImage: `url(${bg})` }}>
+            <section className="hero is-warning">
+                <div className="hero-body">
+                    <div className="container">
+                        <h1 className="title">
+                            Farnborough Guide
+                        </h1>     
+                        <h2 className="subtitle">
+                            The Guide to Your Town
+                        </h2>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+    oldrender() {
+        // const headerImg = '../assets/iStock-504241498.jpg';
+        // const headerImg = 'http://mrmrs.io/photos/u/011.jpg'; //./iStock-504241498.jpg';
+        return (
 
-  render () {
-    // const headerImg = '../assets/iStock-504241498.jpg';
-    // const headerImg = 'http://mrmrs.io/photos/u/011.jpg'; //./iStock-504241498.jpg';
-    return (
-        <header className='bg-center cover w-100 pa3 ph5-ns bg-dark-green-90 sans-serif arms'>
-            
-            <div className='db dt-ns mw9 center w-100'>
-                <h3 className="f2 f1-m f-headline-l measure-narrow lh-title mv0">
-                <span className="bg-black-90 lh-copy white pa1 tracked-tight">
-                    Farnborough Guide
+            <header className='bg-center cover w-100 pa3 ph5-ns bg-dark-green-90 sans-serif arms'>
+
+                <div className='db dt-ns mw9 center w-100'>
+                    <h3 className="f2 f1-m f-headline-l measure-narrow lh-title mv0">
+                        <span className="bg-black-90 lh-copy white pa1 tracked-tight">
+                            Farnborough Guide
                 </span>
-                </h3>
-                <h4 className="f2 lh-copy">
-                <span className="bg-black-90 lh-copy white pa1 i tracked-tight">
-                    The definitive guide to your town.
+                    </h3>
+                    <h4 className="f2 lh-copy">
+                        <span className="bg-black-90 lh-copy white pa1 i tracked-tight">
+                            The definitive guide to your town.
                 </span>
-                </h4>
-                
-            </div>
-        </header> 
-    )
-  }
+                    </h4>
+
+                </div>
+            </header>
+        )
+    }
 }
 
 export default Header
--- src/containers/HomePage/IntroBlock.js
+++ src/containers/HomePage/IntroBlock.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { 
-  Link 
+import {
+  Link
 } from 'react-router'
 
 class IntroBlock extends React.Component {
@@ -8,50 +8,54 @@ class IntroBlock extends React.Component {
   static propTypes = {
     data: React.PropTypes.object,
   }
-  
-  render () {
+
+  render() {
 
     return (
-      <div className="db dt-ns mw9 center">
-        <div className="fl-m fl-l w-33-m w-33-l">
-          <div className="pa4 pa4-m">
-          <h3 className="f3">What's in the name?</h3>
-            <p className="mid-gray f4 lh-copy">            
-              Farnborough was mentioned in the domesday book of 1086, a Saxon settlement. The name Farnborough is derived from "Ferneberga" believed to mean "Fern Hill". There are many links to this ancient name across the town.
-            </p> 
+      <section className="homepage-introblock">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-primary">
+              <h2 className="title">What's in the name?</h2>
+              <div className="content">
+                <p>
+                  Farnborough was mentioned in the domesday book of 1086, a Saxon settlement. The name Farnborough is derived from "Ferneberga" believed to mean "Fern Hill". There are many links to this ancient name across the town.
+                </p>
+              </div>
+            </div>
+            <div className="column">
+              <h2 className="title">Aircraft and Aviation</h2>
+              <div className="content">
+                <p>
+                  Over the years Farnborough has played a major role in the British aviation industry and is linked with three particular milestones in the history of the industry that has ultimately led to faster travel across the globe.
+                </p>
+              </div>
+            </div>
+            <div className="column">
+              <h2 className="title">The Future...</h2>
+              <div className="content">
+                <p>
+                  Farnborough is a town that has always been in a state of transition mainly due to the aviation and related industry here, so it is no surprise that it continues to be attractive to investors in business and property.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="fl-m fl-l w-33-m w-33-l">
-          <div className="pa4 pa4-m">
-            <h3 className="f3">Aircraft and Aviation</h3>
-            <p className="mid-gray f4 lh-copy measure-narrow">
-              Over the years Farnborough has played a major role in the British aviation industry and is linked with three particular milestones in the history of the industry that has ultimately led to faster travel across the globe.  
-            </p>
-          </div>
-        </div>
-        <div className="fl-m fl-l w-33-m w-33-l br b--light-gray">
-          <div className="pa4 pa4-m">
-            <h3 className="f3">The Future...</h3>
-            <p className="mid-gray f4 lh-copy measure-narrow">
-              Farnborough is a town that has always been in a state of transition mainly due to the aviation and related industry here, so it is no surprise that it continues to be attractive to investors in business and property.  
-            </p>
-          </div>
-        </div> 
-      </div>
-    )    
+      </section>
+    )
   }
 
   // Dan, If not logged in show the Register button, otherwise show the view all photos button
   _renderButton() {
-    if ( !this.props.data.user ) {
+    if (!this.props.data.user) {
       return <Link className="f3 fw4 hover-red no-underline black-70 dib pv2 ph3 ba" to="/signup" >Register Now!</Link>
     } else {
-      return <Link className="f3 fw4 hover-red no-underline black-70 dib pv2 ph3 ba" to="/Photo" >View all photos</Link> 
+      return <Link className="f3 fw4 hover-red no-underline black-70 dib pv2 ph3 ba" to="/Photo" >View all photos</Link>
     }
   }
 
   _showText() {
-    if ( !this.props.data.user ) {
+    if (!this.props.data.user) {
       return <h3 className="f3">1. Sign-up</h3>
     } else {
       return <h3 className="f3">2. Add photos</h3>
