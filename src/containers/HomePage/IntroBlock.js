8a7a596ddfbd3645ce07152f10745755894e4bc5
Nick Lewis <nickeblewis@gmail.com>
2017-12-04 17:28:32 +0000

    Made intro text on homepage bigger

--- src/containers/HomePage/IntroBlock.js
+++ src/containers/HomePage/IntroBlock.js
@@ -17,7 +17,7 @@ class IntroBlock extends React.Component {
           <div className="columns">
             <div className="column is-primary">
               <h2 className="title">What's in the name?</h2>
-              <div className="content">
+              <div className="content is-large">
                 <p>
                   Farnborough was mentioned in the domesday book of 1086, a Saxon settlement. The name Farnborough is derived from "Ferneberga" believed to mean "Fern Hill". There are many links to this ancient name across the town.
                 </p>
@@ -25,7 +25,7 @@ class IntroBlock extends React.Component {
             </div>
             <div className="column">
               <h2 className="title">Aircraft and Aviation</h2>
-              <div className="content">
+              <div className="content is-large">
                 <p>
                   Over the years Farnborough has played a major role in the British aviation industry and is linked with three particular milestones in the history of the industry that has ultimately led to faster travel across the globe.
                 </p>
@@ -33,7 +33,7 @@ class IntroBlock extends React.Component {
             </div>
             <div className="column">
               <h2 className="title">The Future...</h2>
-              <div className="content">
+              <div className="content is-large">
                 <p>
                   Farnborough is a town that has always been in a state of transition mainly due to the aviation and related industry here, so it is no surprise that it continues to be attractive to investors in business and property.
                 </p>
